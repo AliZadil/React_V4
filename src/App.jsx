fbc375808a3830020849fd8c3a7c52ea7ba69565
Ali Zadil <tarikz94@gmail.com>
2023-06-07 15:09:26 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         name={<strong>Adrian Antochi</strong>} 
         age={28} 
       />
-      <Welcome name="Ali" age={77}></Welcome>
+      <Welcome name="John" age={77}></Welcome>
     </div>
   );
 }
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Age from './Age';
 
 function Welcome({ name = 'Guest', age }) {
+  const isValidName = name == 'John';
   const isValidAge = age > 18 && age < 65;
   return (
     <div>
-      <p>Welcome, {name}!</p>
+      <p>Welcome, {isValidName ? name : ''}!</p>
       {isValidAge && <Age age={age} />}
     </div>
   );

cd225838b89c69a5ce3906201987d07f5b6c0beb
Ali Zadil <tarikz94@gmail.com>
2023-06-16 17:00:27 +0300

    done

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
 function ToDoList() {
-  const [items, setItems] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([]);
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+  const handleInputChange = (ev) => {
+    setInputValue(ev.target.value);
   };
 
-  const handleAddItem = () => {
+  const handleAddTodo = () => {
     if (inputValue.trim() !== '') {
       setItems((prevItems) => [...prevItems, inputValue]);
       setInputValue('');
@@ -17,18 +17,13 @@ function ToDoList() {
 
   return (
     <div>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <ul>
         {items.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
       </ul>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Enter an item"
-      />
-      <button onClick={handleAddItem}>Add Item</button>
     </div>
   );
 }

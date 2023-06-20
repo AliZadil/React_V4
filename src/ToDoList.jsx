450f946789ecd0683957182534def18aa5545700
Ali Zadil <tarikz94@gmail.com>
2023-06-20 17:50:10 +0300

    done

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,27 +1,49 @@
-import React from "react"
+import React, { Component } from "react";
 
-export default class ToDoList extends React.Component {
-    state = {
-        items: ["Is", "this", "what", "you", "wanted?"],
-        newTodo: "",
-    }
-    addItem = () => {
-        this.setState(state => ({
-            items: [...state.items, state.newTodo],
-        }))
-        this.setState({
-            newTodo: "",
-        })
-    }
-    render() {
-        return <div>
-            <input type="text" name="newTodo" value={this.state.newTodo} onChange={(event) => {
-                this.setState({ newTodo: event.target.value})
-            }} />
-            <button onClick={this.addItem}>Add</button>
-            <ul>
-                {this.state.items.map((item) => <li>{item}</li>)}
-            </ul>
-        </div>
-    }
+export default class ToDoList extends Component {
+  state = {
+    items: ["Is", "this", "what", "you", "wanted?"],
+    newTodo: "",
+  };
+
+  addItem = () => {
+    this.setState((prevState) => ({
+      items: [...prevState.items, prevState.newTodo],
+      newTodo: "",
+    }));
+  };
+
+  resetItems = () => {
+    this.setState({
+      items: [],
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      newTodo: event.target.value,
+    });
+  };
+
+  render() {
+    const { items, newTodo } = this.state;
+
+    return (
+      <div>
+        <input
+          type="text"
+          name="newTodo"
+          value={newTodo}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.addItem}>Add</button>
+        <button onClick={this.resetItems}>Reset</button>
+        <ul>
+          {items.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }

68d299db8a94442c9f4a92aa40d6c236cb50e03c
Ali Zadil <tarikz94@gmail.com>
2023-06-28 20:27:44 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import {Routes, Route, Link} from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 
 import CarDetails from './CarDetails';
 import DisplayLanguage from './DisplayLanguage';
-import {LanguageContext} from "./LanguageContext";
-import CustomCounter from "./CustomCounter";
-import FilteredList from "./FilteredList";
-import Counter from "./counter"
-import InteractiveWelcome from "./InteractiveWelcome";
+import { LanguageContext } from './LanguageContext';
+import CustomCounter from './CustomCounter';
+import FilteredList from './FilteredList';
+import Counter from './Counter';
+import InteractiveWelcome from './InteractiveWelcome';
 
 export default function App() {
-  return (   
+  return (
     <div>
-      <div>
-        <Routes>
-          <Route path="/" element={<InteractiveWelcome />}></Route>
-          <Route path="/counter" element={<Counter></Counter>}></Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<InteractiveWelcome />} />
+        <Route path="/counter" element={<Counter initialValue={10} incrementAmount={2} incrementInterval={500} />} />
+
+      </Routes>
       <div>
         <p>Links</p>
-        <Link to={"/counter"}>Counter</Link>
+        <Link to="/counter">Counter</Link>
+        <br />
+        <Link to="/">Home</Link>
       </div>
     </div>
-  )
+  );
 }

a52ee4a0e6fe8fb8d40cf25547dfbe281c0bf5ea
Ali Zadil <tarikz94@gmail.com>
2023-06-28 19:53:59 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import CarDetails from './CarDetails';
 import DisplayLanguage from './DisplayLanguage';
 import {LanguageContext} from "./LanguageContext";
 import CustomCounter from "./CustomCounter";
+import FilteredList from "./FilteredList";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <CustomCounter />
-      </div>
-    )
-  }
+export default function App() {
+  const list = [
+    { id: 1, name: "John", age: 24 },
+    { id: 2, name: "Emily", age: 32 },
+    { id: 3, name: "Michael", age: 19 },
+    { id: 4, name: "Sarah", age: 27 },
+    { id: 5, name: "Daniel", age: 20 },
+    { id: 6, name: "Sophia", age: 29 },
+  ];
+
+  return (
+    <div className="container">
+      <FilteredList list={list} />
+    </div>
+  );
 }
 
-export default App;

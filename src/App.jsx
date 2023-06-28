7ba49038cd17a3fb9e1bdf4860a5ba46dd3f57b7
Ali Zadil <tarikz94@gmail.com>
2023-06-28 20:35:47 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import CustomCounter from './CustomCounter';
 import FilteredList from './FilteredList';
 import Counter from './Counter';
 import InteractiveWelcome from './InteractiveWelcome';
+import ShowGithubUser from "./ShowGithubUser";
 
 export default function App() {
   return (
@@ -15,13 +16,17 @@ export default function App() {
       <Routes>
         <Route path="/" element={<InteractiveWelcome />} />
         <Route path="/counter" element={<Counter initialValue={10} incrementAmount={2} incrementInterval={500} />} />
-
+        <Route path="/users/:username" element={<ShowGithubUser></ShowGithubUser>} />
       </Routes>
       <div>
         <p>Links</p>
         <Link to="/counter">Counter</Link>
         <br />
         <Link to="/">Home</Link>
+        <br />
+        <Link to={"/users/AliZadil"}>My Github Profile</Link>
+        <br />
+        <Link to={"/users/doublemacro"}>Adrian</Link>
       </div>
     </div>
   );

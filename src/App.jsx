26b1da67aa64bff3892c65031e3a804761ede614
Ali Zadil <tarikz94@gmail.com>
2023-06-28 20:06:25 +0300

    not done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
+import {Routes, Route, Link} from "react-router-dom";
+
 import CarDetails from './CarDetails';
 import DisplayLanguage from './DisplayLanguage';
 import {LanguageContext} from "./LanguageContext";
 import CustomCounter from "./CustomCounter";
 import FilteredList from "./FilteredList";
+import Counter from "./counter"
+import InteractiveWelcome from "./InteractiveWelcome";
 
 export default function App() {
-  const list = [
-    { id: 1, name: "John", age: 24 },
-    { id: 2, name: "Emily", age: 32 },
-    { id: 3, name: "Michael", age: 19 },
-    { id: 4, name: "Sarah", age: 27 },
-    { id: 5, name: "Daniel", age: 20 },
-    { id: 6, name: "Sophia", age: 29 },
-  ];
-
-  return (
-    <div className="container">
-      <FilteredList list={list} />
+  return (   
+    <div>
+      <div>
+        <Routes>
+          <Route path="/" element={<InteractiveWelcome />}></Route>
+          <Route path="/counter" element={<Counter></Counter>}></Route>
+        </Routes>
+      </div>
+      <div>
+        <p>Links</p>
+        <Link to={"/counter"}>Counter</Link>
+      </div>
     </div>
-  );
+  )
 }
-

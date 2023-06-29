12a8086833edc2ba56b8519da0112263b6b24d92
Ali Zadil <tarikz94@gmail.com>
2023-06-29 12:24:55 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
 
 import CarDetails from './CarDetails';
 import DisplayLanguage from './DisplayLanguage';
@@ -8,8 +8,8 @@ import CustomCounter from './CustomCounter';
 import FilteredList from './FilteredList';
 import Counter from './Counter';
 import InteractiveWelcome from './InteractiveWelcome';
-import ShowGithubUser from "./ShowGithubUser";
-import GithubUserList from "./GithubUserList";
+import ShowGithubUser from './ShowGithubUser';
+import GithubUserList from './GithubUserList';
 
 export default function App() {
   return (
@@ -17,8 +17,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<InteractiveWelcome />} />
         <Route path="/counter" element={<Counter initialValue={10} incrementAmount={2} incrementInterval={500} />} />
-        <Route path="/users" element={<GithubUserList />}></Route>
-        <Route path="/users/:username" element={<ShowGithubUser></ShowGithubUser>} />
+        <Route path="users" element={<GithubUserList />}>
+          <Route index element={<h2>Add a user and select it</h2>} />
+          <Route path=":username" element={<ShowGithubUser />} />
+        </Route>
         <Route path="/*" element={<h1>404 Page Not Found</h1>} />
       </Routes>
       <div>
@@ -27,12 +29,9 @@ export default function App() {
         <br />
         <Link to="/">Home</Link>
         <br />
-        <Link to={"/users/AliZadil"}>My Github Profile</Link>
-        <br />
-        <Link to={"/users/doublemacro"}>Adrian</Link>
-        <br />
-        <Link to={"/users"}>GitHub Users</Link>
+        <Link to="/users">GitHub Users</Link>
       </div>
+      <Outlet />
     </div>
   );
 }
--- src/GithubUserList.jsx
+++ src/GithubUserList.jsx
@@ -1,31 +1,39 @@
-import React, { useState } from 'react';
-import GithubUser from './GithubUser';
+import React, { useState } from "react";
+import { Link, Route, Routes, Outlet } from "react-router-dom";
+import ShowGithubUser from "./ShowGithubUser";
 
-function GithubUserList() {
-  const [usernames, setUsernames] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+export default function GithubUserList() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+  const handleInput = (event) => {
+    setInput(event.target.value);
   };
 
-  const handleAddUser = () => {
-    setUsernames((prevUsernames) => [...prevUsernames, inputValue]);
-    setInputValue('');
+  const handleList = () => {
+    setList((arr) => [...arr, input]);
+    setInput("");
   };
 
   return (
     <div>
+      <input
+        onChange={handleInput}
+        placeholder="Enter a GitHub username"
+        value={input}
+      />
+      <button onClick={handleList}>Add User</button>
+      <button onClick={() => setList([])}>Clear</button>
+
       <div>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button onClick={handleAddUser}>Add User</button>
+        {list.map((user) => (
+          <div key={user}>
+            <Link to={`/users/${user}`}>{user}</Link>
+          </div>
+        ))}
       </div>
 
-      {usernames.map((username, index) => (
-        <GithubUser key={index} username={username} />
-      ))}
+      <Outlet />
     </div>
   );
 }
-
-export default GithubUserList;
